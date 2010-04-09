2d0b255e31e895b9ae747d63843ac1de9f030ae5
admin <admin@091412ce-188b-8647-ba9f-17b733bc8b83>
2010-04-09 16:06:31 +0000

    My plugin - editable support for DataTables.
    git-svn-id: http://motik.ru/svn/mainrepo/js/branches/jquery@1001 091412ce-188b-8647-ba9f-17b733bc8b83

---
+++ jquery/jquery.dteditable.js
@@ -0,0 +1,97 @@
+/*
+ * DTEditable - jQuery in place edit plugin for DataTables ( http://datatables.net )
+ *
+ * Copyright (c) 2010 mot <2matvei@gmail.com>
+ *
+ * Licensed under the GPLv3 license:
+ *   http://www.opensource.org/licenses/gpl-3.0.html
+ *
+ * Project home:
+ *   http://www.matvei.ru
+ *
+ * Inspired by Jeditable by Mika Tuupola, Dylan Verheul
+ *    http://www.appelsiini.net/projects/jeditable
+ *
+ */
+/**
+  * Version 0.1
+  *
+  * @name  DTEditable
+  * @type  jQuery
+  * @param String  target             (POST) URL or function to send edited content to **
+  * @param Hash    options            additional options
+  * @param Function options[callback] Function to run after submitting edited content **
+  * @param Hash    options[submitdata] Extra parameters to send when submitting edited content.
+  *
+  */
+
+(function($) {
+
+    $.fn.editable = function(target, settings) {
+    	this.live('click.editable', function(e) {
+    		if ($(this).data('bEditing')) return;
+
+            e.preventDefault();
+            e.stopPropagation();
+
+    		$(this).data('sOldText', this.innerHTML)
+    			.html('<input type="text" value="' + this.innerHTML + '" />')
+    			.data('bEditing', true)
+    			.children().focus();
+
+    		$.editable.setTimeout(this);
+    	});
+
+    	this.live('keydown', function(e) {
+    		if (!$(this).data('bEditing')) return;
+
+    		if (e.which == 13) {//enter
+    			$.editable.clearTimeout(this);
+
+    			var oSubmitData = {
+    				id: this.id,
+    				value: e.target.value
+    			};
+
+    			if (settings.submitdata) {
+    				if ($.isFunction(settings.submitdata)) {
+                        $.extend(oSubmitData, settings.submitdata.call(this));
+                    }
+    				else {
+    					$.extend(oSubmitData, settings.submitdata);
+                    }
+    			}
+
+    			$.post(target, oSubmitData, $.proxy(function (sText) {
+    					$.editable.setText(this, sText);
+
+    					if ($.isFunction(settings.callback)) {
+    						settings.callback.call(this, sText);
+    					}
+    				}, this));
+    		}
+    		else if (e.which == 27) {//esc
+    			$.editable.clearTimeout(this);
+    			$.editable.setText(this);
+    		}
+    		else {//all other
+    			$.editable.setTimeout(this);
+    		}
+    	});
+    };
+
+    $.editable = {
+   		setTimeout: function(e) {
+    		$.editable.clearTimeout(e);
+
+    		$(e).data('iTimeoutID', setTimeout(function() {$.editable.setText(e);}, 2000));
+    	},
+    	clearTimeout: function(e) {
+    		clearTimeout($(e).data('iTimeoutID'));
+    	},
+    	setText: function(e, sText) {
+    		$(e).html(sText || $(e).data('sOldText')).removeData('bEditing');
+    	}
+    };
+
+})(JQuery);
