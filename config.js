83bf05e666f1475c31e5d68e34b5d362964c27cd
admin <admin@091412ce-188b-8647-ba9f-17b733bc8b83>
2010-04-10 16:10:14 +0000

    New file for default configs.
    git-svn-id: http://motik.ru/svn/mainrepo/js/branches/jquery@1012 091412ce-188b-8647-ba9f-17b733bc8b83

---
+++ config.js
@@ -0,0 +1,31 @@
+/*
+ * Store config here
+ *
+ */
+
+oDataTableDefaults = {
+	"aaSorting": [[1, 'asc']],
+	//<"top"i>rt<"bottom"flp<"clear">
+	"sDom": '<"top"<"dtBar">fi>rt<"bottom"lp<"clear">',
+	"bStateSave": true,
+	iCookieDuration: 86400,//one day
+	sPaginationType: 'full_numbers',
+	"oLanguage": {
+		"sLengthMenu": "Выводить _MENU_ записей на страницу",
+		"sProcessing": 'Работаем с данными',
+		"sZeroRecords": "Нет такой буквы",
+		"sInfo": "Вывожу с _START_ по _END_ из _TOTAL_ записей",
+		"sInfoEmtpy": "Вывожу с 0 по 0 из 0 записей",
+		"sInfoFiltered": "(найдено из _MAX_ записей)",
+		"sInfoPostFix": "",
+		"sSearch": "Поиск:",
+		"oPaginate": {
+			"sFirst": "Первая",
+			"sLast": "Последняя",
+			"sNext": ">",
+			"sPrevious": "<"
+		}
+	},
+	"bProcessing": true,
+	"bJQueryUI": false
+};
