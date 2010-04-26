bb11f047ea4c7ee71a5eff6f2f698a053a03e398
admin <admin@091412ce-188b-8647-ba9f-17b733bc8b83>
2010-04-26 12:31:57 +0000

    Added history support.
    git-svn-id: http://motik.ru/svn/mainrepo/js/branches/jquery@1127 091412ce-188b-8647-ba9f-17b733bc8b83

--- jquery/jquery.tabs.js
+++ jquery/jquery.tabs.js
@@ -24,7 +24,6 @@
 
 /*
  * TODO:
- * port history plugin
  *
  */
 
@@ -39,12 +38,12 @@
 
 		this.data('tabs', true);
 
-		this.click(function(e, bNoLocationChange) {
+		this.click(function(e, bWaitBeforeChange) {
 			var oA = $(e.target);
 			//if not href
 			if (!oA.is('a')) return;
 
-			var sPath = oA.attr('href');
+			var sPath = oA.attr('href').replace("#", "");//actually we do not need this replace, left for compatibility
 
 			if (!sPath) return;
 
@@ -54,11 +53,26 @@
 
 			oA.parent().addClass('current').siblings().removeClass('current');
 
-			//start history plugin
-			if (!bNoLocationChange) {
-				//actually we do not need this replace, left for comapatibility
-				location.hash = sPath.replace("#", "");
+			//be neat, clean if we had one before
+			if ($.tabs.iTimeOutID) {
+				//we clean timeout because we do not want to get first-level tab sPath as history point
+				clearTimeout($.tabs.iTimeOutID);
+				$.tabs.iTimeOutID = null;
 			}
+
+			//if we have first load trigger event
+			if (bWaitBeforeChange) {
+				//we do so because we can have lower level tabs, which need original location.hash.
+				//but we need to change location.hash for history plugin support, if no lower tabs found.
+				$.tabs.iTimeOutID = setTimeout(function() { location.hash = sPath; }, 1000);
+			}
+			//change location hash in a moment on natural click event
+			else {
+				location.hash = sPath;
+			}
+
+			//store current path, used for history support
+			$(this).data('currentPath', sPath);
 
 			return false;
 		});
@@ -66,13 +80,31 @@
 		//try to open tab from location
 		this.find('a').each(function(i, element) {
 			//if location.hash is empty, it simple matches the first href
-			if ($(this).attr('href').search(location.hash.replace("#", "")) > -1 || location.hash.replace("#", "").search($(this).attr('href')) > -1) {
-				$(this).trigger('click', [true]);//send true not to change location, because it can be used by lower level tabs
+			if ($(this).attr('href').search(location.hash.replace("#", "")) > -1) {
+				$(this).trigger('click');
+				return false;
+			}
+			else if (location.hash.replace("#", "").search($(this).attr('href')) > -1) {//if href is only part of location
+				$(this).trigger('click', [true]);//send true to change location later, because it can be used by lower level tabs
 				return false;
 			}
 		});
 
+		//history support
+		setInterval($.proxy(function() {
+			var h = location.hash.replace("#", "");
+			var oA = this.find('a').filter("[href=" + h + "]");
+
+			if (oA.length && h !== this.data('currentPath')) {
+				oA.trigger('click');
+			}
+		}, this), 200);
+
 		return this;
+	};
+
+	$.tabs = {
+		iTimeOutID: null
 	};
 
 })(jQuery);
