d14e5a801ec21c765c99d8bdd0656affe74246f6
Matvei Fisenko <2matvei@gmail.com>
2010-10-27 00:05:24 +0400

    Added new _Defaults object to handle all default config.

--- config.js
+++ config.js
@@ -29,3 +29,20 @@ var oDataTableDefaults = {
 	"bJQueryUI": false,
 	"bAutoWidth": false
 };
+
+var _Defaults = {
+
+	DataTable: oDataTableDefaults,
+
+	Tooltip: {
+		// place tooltip on the right edge
+		position: "center right",
+		// a little tweaking of the position
+		offset: [-2, 10],
+		// use the built-in fadeIn/fadeOut effect
+		effect: "fade",
+		// custom opacity setting
+		opacity: 0.7
+	}
+
+};
